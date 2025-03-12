2cebc1b43c5858a324dbbde5d6a7b872aa8ef430
Kadir Yavuz Kurt <k.yavuzkurt1@gmail.com>
2025-03-12 14:53:30 +0300

    Changed UR10 Reach enviroment to UR3 Reach enviroment.

--- source/isaaclab_assets/isaaclab_assets/robots/universal_robots.py
+++ source/isaaclab_assets/isaaclab_assets/robots/universal_robots.py
@@ -15,7 +15,7 @@ Reference: https://github.com/ros-industrial/universal_robot
 import isaaclab.sim as sim_utils
 from isaaclab.actuators import ImplicitActuatorCfg
 from isaaclab.assets.articulation import ArticulationCfg
-from isaaclab.utils.assets import ISAACLAB_NUCLEUS_DIR
+from isaaclab.utils.assets import ISAACLAB_NUCLEUS_DIR, ISAAC_NUCLEUS_DIR
 
 ##
 # Configuration
@@ -52,3 +52,34 @@ UR10_CFG = ArticulationCfg(
     },
 )
 """Configuration of UR-10 arm using implicit actuator models."""
+
+UR3_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=f"{ISAAC_NUCLEUS_DIR}/Robots/UniversalRobots/ur3/ur3.usd",
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            max_depenetration_velocity=5.0,
+        ),
+        activate_contact_sensors=False,
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        joint_pos={
+            "shoulder_pan_joint": 0.0,
+            "shoulder_lift_joint": -1.712,
+            "elbow_joint": 1.712,
+            "wrist_1_joint": 0.0,
+            "wrist_2_joint": 0.0,
+            "wrist_3_joint": 0.0,
+        },
+    ),
+    actuators={
+        "arm": ImplicitActuatorCfg(
+            joint_names_expr=[".*"],
+            velocity_limit=100.0,
+            effort_limit=28.0,
+            stiffness=400.0,
+            damping=20.0,
+        ),
+    },
+)
+"""Configuration of UR-3 arm using implicit actuator models."""
--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/reach/config/ur_10/lightweight_play_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/reach/config/ur_10/lightweight_play_cfg.py
@@ -6,6 +6,7 @@
 import math
 
 from isaaclab.utils import configclass
+from isaaclab.sim import PhysxCfg
 
 import isaaclab_tasks.manager_based.manipulation.reach.mdp as mdp
 from isaaclab_tasks.manager_based.manipulation.reach.reach_env_cfg import ReachEnvCfg
@@ -22,40 +23,49 @@ from isaaclab_assets import UR3_CFG  # isort: skip
 
 
 @configclass
-class UR10ReachEnvCfg(ReachEnvCfg):
+class UR10ReachEnvCfg_UltraLight(ReachEnvCfg):
     def __post_init__(self):
         # post init of parent
         super().__post_init__()
 
-        # Reduce the number of environments even further to prevent memory issues
-        self.scene.num_envs = 256  # Try a much smaller number
+        # Minimal environment count for visualization
+        self.scene.num_envs = 1  # Just a single environment
+        self.scene.env_spacing = 2.0  # Reduced spacing since we have only one env
+
+        # Low-quality physics simulation for performance
+        self.sim = PhysxCfg(
+            dt=0.02,  # Larger physics timestep
+            substeps=1,  # Minimum substeps
+            use_gpu_pipeline=False,  # Disable GPU physics
+            use_fabric=False,  # Disable fabric
+        )
 
         # switch robot to ur3
         self.scene.robot = UR3_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+        
         # override events
         self.events.reset_robot_joints.params["position_range"] = (0.75, 1.25)
+        
         # override rewards - change ee_link to wrist_3_link
         self.rewards.end_effector_position_tracking.params["asset_cfg"].body_names = ["wrist_3_link"]
         self.rewards.end_effector_position_tracking_fine_grained.params["asset_cfg"].body_names = ["wrist_3_link"]
         self.rewards.end_effector_orientation_tracking.params["asset_cfg"].body_names = ["wrist_3_link"]
+        
         # override actions
         self.actions.arm_action = mdp.JointPositionActionCfg(
             asset_name="robot", joint_names=[".*"], scale=0.5, use_default_offset=True
         )
+        
         # override command generator body - change ee_link to wrist_3_link
-        # end-effector is along x-direction
         self.commands.ee_pose.body_name = "wrist_3_link"
         # Keep existing pitch range configuration
         self.commands.ee_pose.ranges.pitch = (math.pi / 2, math.pi / 2)
 
+        # Disable all visualizations to improve performance
+        self.visualizer.enable = False
 
-@configclass
-class UR10ReachEnvCfg_PLAY(UR10ReachEnvCfg):
-    def __post_init__(self):
-        # post init of parent
-        super().__post_init__()
-        # make a smaller scene for play
-        self.scene.num_envs = 50
-        self.scene.env_spacing = 2.5
-        # disable randomization for play
-        self.observations.policy.enable_corruption = False
+        # Disable terrain and other complex scene elements
+        self.scene.terrain.enable = False
+
+        # Disable randomization for play to reduce compute
+        self.observations.policy.enable_corruption = False 
